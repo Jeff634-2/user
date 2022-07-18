780cc75ae998cdd479dcb1588da98c6de21eb8fe
Jeff <13631236809@163.com>
2022-07-18 18:00:08 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"user/handler"
+	pb "user/proto"
+
+	"github.com/micro/micro/v3/service"
+	"github.com/micro/micro/v3/service/logger"
+)
+
+func main() {
+	// Create service
+	srv := service.New(
+		service.Name("user"),
+	)
+
+	// Register handler
+	pb.RegisterUserHandler(srv.Server(), handler.New())
+
+	// Run service
+	if err := srv.Run(); err != nil {
+		logger.Fatal(err)
+	}
+}
